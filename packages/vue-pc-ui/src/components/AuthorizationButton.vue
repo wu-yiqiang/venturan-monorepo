04e17cf4b4fb18b6e9e27684797d8e2b1b1fcfa4
wu_yiqiang <sutter.wu@itforce-tech.com>
2025-07-11 17:13:09 +0800

    添加权限按钮

---
+++ packages/vue-pc-ui/src/components/AuthorizationButton.vue
@@ -0,0 +1,24 @@
+<template>
+	<section class='AuthorizationButton'>
+		<slot v-if="isAuth" />
+	</section>
+</template>
+
+<script setup lang='ts'>
+const props = defineProps({
+	code: {
+		type: String,
+		default: '100%'
+	},
+})
+const isAuth = computed(() => {
+	const auths = ['system:user:edit']
+	if (auths.includes(props?.code)){ return true}
+	return false
+})
+</script>
+<style scoped lang='scss'>
+.AuthorizationButton {
+
+}
+</style>
